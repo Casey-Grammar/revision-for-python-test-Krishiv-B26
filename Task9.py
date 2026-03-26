24bf72c7672a9a484c8606fc094fc870e496cb78
Krishiv-B26 <s30160@caseygrammar.vic.edu.au>
2026-03-26 01:42:03 +0000

    Completed Task 9

--- Task9.py
+++ Task9.py
@@ -5,7 +5,10 @@
 
 def main():
     #Write your code here
-    
+    list_of_cats_names = input("Cats: ")
+    list_of_cats_names = list_of_cats_names.split()
+    print("You have " + str(len(list_of_cats_names)) + " cats.")
+     
 
 
 
