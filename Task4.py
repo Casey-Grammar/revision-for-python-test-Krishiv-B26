4141bfc2e51f88af98c94a68648ebbf36432b81b
Krishiv-B26 <s30160@caseygrammar.vic.edu.au>
2026-03-26 01:05:00 +0000

    Completed Task 4

--- Task4.py
+++ Task4.py
@@ -5,7 +5,11 @@
 
 def main():
     #Write your code here
-    
+    password = input("What is the password Ali? ")
+    if password == 'Open sesame!':
+        print("The cave door opens!")
+    else:
+        print("Nothing happens.")
 
 
     # End of your code here
