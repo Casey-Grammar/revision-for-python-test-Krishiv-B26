c8152806dc5327918f2cb40758d212f0a573341d
Krishiv-B26 <s30160@caseygrammar.vic.edu.au>
2026-03-26 01:26:14 +0000

    Completed Task 7

--- Task7.py
+++ Task7.py
@@ -7,7 +7,12 @@
 
 def main():
     #Write your code here
-    
+    time_to_launch = int(input("Time to launch: "))
+    print("Counting down ...")
+    for i in range(time_to_launch, 0, -1):
+        print(f"{i} ...")
+    print("Blast off!")
+
 
     
     # End of your code here
